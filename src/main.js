0b53080378787913e06e7f18ca806627b02352bd
Paige Williams and Tanvi Garg <tanvi@>
2019-01-16 13:26:13 -0800

    Refactor code to move promise into function.

--- src/main.js
+++ src/main.js
@@ -5,17 +5,26 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-// function attachClickListener(){
-//   $('ul#dino').on("click", "li", function(){
-//    let number = this.id;
-//   })
-// }
+function attachClickListener(promise){
+  $('ul#dino').on("click", "li", function(){
+    let dinoId = this.id;
+    console.log("before: " + dinoId);
+
+    promise.then(function(response){
+      let body = JSON.parse(response);
+      console.log(dinoId);
+      let specificGif = body.data[dinoId].embed_url
+      $("#gif").html(`<iframe src="${specificGif}" width="480" height="327" frameBorder="0" allowFullScreen></iframe>`);
+    }, function(error){
+      $(".gifError").text(`There was error processing your request: ${error.message}`);
+    });
+  });
+}
 
 $(document).ready(function(){
 
   let dinoObject = new Dino();
   let dinoPromise = dinoObject.getDino();
-
 
   dinoPromise.then(function(response){
     let body = JSON.parse(response);
@@ -23,27 +32,13 @@ $(document).ready(function(){
     array.forEach(function(name, index){
       $("#dino").append(`<li id=${index}>${name}</li>`);
     });
-     //attachClickListener();
-
   }, function(error){
     $(".dinoError").text(`There was an error processing your request: ${error.message}`);
   });
 
-
-$('ul#dino').on("click", "li", function(){
   let giphyObject = new Giphy();
   let giphyPromise = giphyObject.getGiph();
-  let dinoId = this.id;
-  console.log("before: " + dinoId);
-  giphyPromise.then(function(response){
-    let body = JSON.parse(response);
-    console.log(dinoId);
-    let specificGif = body.data[dinoId].embed_url
-    $("#gif").html(`<iframe src="${specificGif}" width="480" height="327" frameBorder="0" allowFullScreen></iframe>`);
-  }, function(error){
-    $(".gifError").text(`There was error processing your request: ${error.message}`);
-  });
-});
 
+  attachClickListener(giphyPromise);
 
 })
