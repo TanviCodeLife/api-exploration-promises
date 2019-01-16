728493da48de7862c57bfc51bd4a009bc887ec01
Paige Williams and Tanvi Garg <tanvi@>
2019-01-16 13:18:09 -0800

    Add giphy promise in click listener.

--- src/main.js
+++ src/main.js
@@ -5,18 +5,13 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+// function attachClickListener(){
+//   $('ul#dino').on("click", "li", function(){
+//    let number = this.id;
+//   })
+// }
+
 $(document).ready(function(){
-
-  let giphyObject = new Giphy();
-  let giphyPromise = giphyObject.getGiph();
-
-  giphyPromise.then(function(response){
-    let body = JSON.parse(response);
-    let specificGif = body.data[24].embed_url
-    $("#gif").append(`<iframe src="${specificGif}" width="480" height="327" frameBorder="0" allowFullScreen></iframe>`);
-  }, function(error){
-    $(".gifError").text(`There was error processing your request: ${error.message}`);
-  });
 
   let dinoObject = new Dino();
   let dinoPromise = dinoObject.getDino();
@@ -25,12 +20,30 @@ $(document).ready(function(){
   dinoPromise.then(function(response){
     let body = JSON.parse(response);
     let array = body[0];
-    console.log(array);
     array.forEach(function(name, index){
       $("#dino").append(`<li id=${index}>${name}</li>`);
     });
+     //attachClickListener();
+
   }, function(error){
     $(".dinoError").text(`There was an error processing your request: ${error.message}`);
   });
+
+
+$('ul#dino').on("click", "li", function(){
+  let giphyObject = new Giphy();
+  let giphyPromise = giphyObject.getGiph();
+  let dinoId = this.id;
+  console.log("before: " + dinoId);
+  giphyPromise.then(function(response){
+    let body = JSON.parse(response);
+    console.log(dinoId);
+    let specificGif = body.data[dinoId].embed_url
+    $("#gif").html(`<iframe src="${specificGif}" width="480" height="327" frameBorder="0" allowFullScreen></iframe>`);
+  }, function(error){
+    $(".gifError").text(`There was error processing your request: ${error.message}`);
+  });
+});
+
 
 })
