77c5ca9460f55723b01f5006b4c0ac126e741cad
Paige Williams and Tanvi Garg <tanvi@>
2019-01-16 10:13:46 -0800

    Create Giphy Promise.

---
+++ src/giphy.js
@@ -0,0 +1,17 @@
+class Giphy {
+  getGiph(index){
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.giphy.com/v1/gifs/search?q=dinosaurs&api_key=${process.env.GIPHY_API_KEY}&limit=25`;
+      request.onload = function(){
+        if(this.status === 200){
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
